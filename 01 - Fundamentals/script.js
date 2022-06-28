13a217d64e8647403aa2d9785613f8b13c402d2f
Troy Arnold <troylarnold@gmail.com>
2022-06-28 15:27:49 -0700

    Variables

--- 01 - Fundamentals/script.js
+++ 01 - Fundamentals/script.js
@@ -1,3 +1,12 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript tutorial");
 console.log(100 + 200 + 300);
+
+console.log("Troy");
+
+let firstName = "Troy";
+let weight = 160;
+
+console.log(firstName);
+console.log(`${firstName}'s weight is ${weight} pounds.`);
+
+let PI = 3.1415;
