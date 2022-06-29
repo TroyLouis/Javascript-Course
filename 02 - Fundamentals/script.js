ede0bb25fb2c47fa5654a0fa8f56eb1ab4093e79
Troy Arnold <troylarnold@gmail.com>
2022-06-29 16:40:05 -0700

    Functions, arrow functions, strict

--- 02 - Fundamentals/script.js
+++ 02 - Fundamentals/script.js
@@ -1,4 +1,31 @@
 "use strict";
 
-let isRealtor = false;
-const passTest = true;
+function logger() {
+  console.log("My name is Troy.");
+}
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+
+function calcAge(birthYear) {
+  const year = 2022;
+  return year - birthYear;
+}
+
+const calcAge2 = (birthYear) => 2022 - birthYear;
+
+const yearsUntilRetire = (birthYear) => {
+  const age = 2022 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+};
+
+const nameConcat = (first, last) => {
+  return `${first} ${last}`;
+};
+
+console.log(nameConcat("Troy", "Smith"));
