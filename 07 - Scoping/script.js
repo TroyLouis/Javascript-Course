85f5cde9046bad26cc60d238fb739bfacc3a97a7
Troy Arnold <troylarnold@gmail.com>
2022-08-02 17:02:21 -0700

    function scope

--- 07 - Scoping/script.js
+++ 07 - Scoping/script.js
@@ -12,3 +12,26 @@ function calcAge(birth) {
 
 const name = "troy";
 console.log(calcAge(1994));
+
+//hoisting
+
+console.log(a); // undefined as exepcted
+// console.log(b);  cannot access before initialization
+// console.log(c);  cannot access before initialization
+var a = "a";
+let b = "b";
+const c = "c";
+
+console.log(addDecl(1, 2)); // no error
+// console.log(addExpr(1, 2)); cannot access before initialization
+// console.log(addArrow(1, 2)); cannot access before initialization
+// temporal dead zone, google it
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
