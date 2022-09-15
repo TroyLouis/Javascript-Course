d9f8cb6c9a3e5a507d8e3c2cfbdf0c4535081763
Troy Arnold <troylarnold@gmail.com>
2022-09-14 17:26:01 -0700

    OOP coding challenge 1

--- 12 - jsOOP/script.js
+++ 12 - jsOOP/script.js
@@ -25,5 +25,38 @@ jonas.calcAge();
 
 console.log(jonas.__proto__);
 console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.__proto__ === Person.prototype);
+console.log(jonas.__proto__ === Person.prototype);
 console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas.species);
+console.log(jonas.hasOwnProperty("firstName"));
+// property species isn't inside of the object, it has access due to the prototype Person
+console.log(jonas.hasOwnProperty("species"));
+
+const arr = [3, 6, 9, 12];
+
+//coding challenge number 1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+const ferarri = new Car("Ferrari", 100);
+console.log(ferarri);
+ferarri.accelerate();
+
+Car.prototype.brake = function (value) {
+  this.speed -= value;
+  console.log(this.speed);
+};
+
+const porsche = new Car("Porsche", 150);
+
+porsche.brake(25);
